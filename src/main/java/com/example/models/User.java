1f4619e3c46e98aaa1a7453121d7715210fa5691
necronomicus <47306591+necronomicus@users.noreply.github.com>
2020-05-31 18:13:35 -0300

    anadirServicioUser

---
+++ src/main/java/com/example/models/User.java
@@ -0,0 +1,45 @@
+package com.example.models;
+
+public class User {
+	private int id;
+	private int dni;
+	private String name;
+	private String lastName;
+	private int sex;
+	
+	
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getDni() {
+		return dni;
+	}
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public int getSex() {
+		return sex;
+	}
+	public void setSex(int sex) {
+		this.sex = sex;
+	}
+	
+}
