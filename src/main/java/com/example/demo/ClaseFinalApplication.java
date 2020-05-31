6877971a233bc777561402323ac7beede82a8dfb
necronomicus <47306591+necronomicus@users.noreply.github.com>
2020-05-31 17:19:57 -0300

    añadiendo modelo

---
+++ src/main/java/com/example/demo/ClaseFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaseFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaseFinalApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/model/User.java
@@ -0,0 +1,40 @@
+package com.example.model;
+
+public class User {
+	private int id;
+	private int dni;
+	private String nombre;
+	private String apellido;
+	private int sexo;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getDni() {
+		return dni;
+	}
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public int getSexo() {
+		return sexo;
+	}
+	public void setSexo(int sexo) {
+		this.sexo = sexo;
+	}
+	
+}
